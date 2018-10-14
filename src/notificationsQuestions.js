eb2c94ef348225e8abe7d4b2681458fb32c262e7
m.navarro <m.navarro@payvision.com>
2018-10-14 20:36:04 +0200

    New notifications questions

--- src/notificationsQuestions.js
+++ src/notificationsQuestions.js
@@ -1,3 +1,6 @@
+const ora = require('ora')
+const { bulkSendEmails, bulkSendPush } = require('send-notifications')
+
 module.exports = {
   questions: {
     type: 'checkbox',
@@ -7,11 +10,36 @@ module.exports = {
       {
         name: 'Mandar emails',
         checked: true,
+        value: sendEmails,
       },
       {
         name: 'Mandar notificaciones PUSH',
         checked: true,
+        value: sendPush,
       },
     ],
   },
+}
+
+async function sendEmails() {
+  const spinner = ora('Enviando emails').start()
+  try {
+    await bulkSendEmails()
+    spinner.succeed('Emails enviados')
+    return true
+  } catch (err) {
+    spinner.fail(err.message)
+    return err
+  }
+}
+
+async function sendPush() {
+  const spinner = ora('Enviando notificaciones Push').start()
+  try {
+    await bulkSendPush()
+    spinner.succeed('Notificaciones push enviadas')
+  } catch (err) {
+    spinner.fail(err.message)
+    return err
+  }
 }
